60b116d54b97712e5a5dac9209d37837953830d2
antoine-hrvt <121826696+antoine-hrvt@users.noreply.github.com>
2023-12-24 17:14:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,10 +10,3 @@ int main(){
     Play_Game();
     return 0;
 }
-
-/**
- * @brief Good evening, sir. Here is my work. I just wanted to inform you that my program is not functioning properly. 
- *        I have a segmentation fault in my Place_Boats function, which prevents the player from completing more than 
- *        2 turns, and I am unable to find the error despite spending several days on it.
- */
-
